4d151128200f6fe9fb6cf2b4a9428d8eb8d82e8d
CaglarTaha <mtaha.mtal@gmail.com>
2022-08-31 14:45:55 +0300

    First Commit
    First Commit and Change Something

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import {useState,useEffect} from "react"
+
+import Loading from './Component/Loading';
+
+
+
 function App() {
+
+ const url = 'https://course-api.com/react-tabs-project'
+
+
+const [loading,setLoading] = useState(true);
+
+
+const [item,setitem] = useState([]);
+
+
+const [value,setValue]= useState(0);
+
+
+
+const  fetchitem  = async () => {
+
+  const response =  await fetch(url);
+
+  const newitem = await response.json();
+
+console.log(newitem)
+  setitem(newitem);
+  setLoading(false);
+
+} 
+
+
+useEffect(() => {
+  fetchitem();
+  
+}, []);
+
+if (loading) {
   return (
+    <Loading></Loading>
+  )
+}
+   
+const {company,dates,duties,title} = item[value]
+  return (
+
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<div className='container'>
+<div className='Tittle'>
+  <h2>Experience</h2>
+  <div className='underline'></div>
+</div>
+<div className='center'>
+  <div className='Row'> 
+  <div className='left'>
+  {item.map ((items,index)=>{
+    return <div key={items.id}>
+      <button onClick={()=>setValue(index)} >{items.company}</button>
+    </div>
+   })}
+  </div>
+  <article className='info'>
+    <h3>{title}</h3>
+    <h4>{company}</h4>
+    <h5>{dates}</h5>
+   {duties.map ((duty,index)=>{
+    return <div key={index}>
+      <p>{duty}</p>
+    </div>
+   })}
+  </article>
+
+  
+  </div>
+</div>
+</div>
+  
     </div>
   );
 }
 
 export default App;
+ 
---
+++ src/Component/Loading.jsx
@@ -0,0 +1,23 @@
+
+
+
+
+const Loading = () => {
+
+
+return(
+    <div>
+<div className="loading-container">
+    <h1 className="Loadinng"></h1>
+    </div>
+    </div>
+)
+
+
+
+}
+
+
+
+
+export default Loading;
